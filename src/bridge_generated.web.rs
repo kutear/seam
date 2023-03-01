55aa7a48f6fa0fb54a85748a2264c657b4a44cd4
KuTear <kutear@kutear.com>
2023-03-01 23:33:26 +0800

    feat: export for library

---
+++ src/bridge_generated.web.rs
@@ -0,0 +1,52 @@
+use super::*;
+// Section: wire functions
+
+#[wasm_bindgen]
+pub fn wire_say_hello(port_: MessagePort) {
+    wire_say_hello_impl(port_)
+}
+
+#[wasm_bindgen]
+pub fn wire_get_room_stream(port_: MessagePort, platform: String, rid: String) {
+    wire_get_room_stream_impl(port_, platform, rid)
+}
+
+#[wasm_bindgen]
+pub fn wire_get_room_danmaku(port_: MessagePort, platform: String, rid: String) {
+    wire_get_room_danmaku_impl(port_, platform, rid)
+}
+
+// Section: allocate functions
+
+// Section: related functions
+
+// Section: impl Wire2Api
+
+impl Wire2Api<String> for String {
+    fn wire2api(self) -> String {
+        self
+    }
+}
+
+impl Wire2Api<Vec<u8>> for Box<[u8]> {
+    fn wire2api(self) -> Vec<u8> {
+        self.into_vec()
+    }
+}
+// Section: impl Wire2Api for JsValue
+
+impl Wire2Api<String> for JsValue {
+    fn wire2api(self) -> String {
+        self.as_string().expect("non-UTF-8 string, or not a string")
+    }
+}
+impl Wire2Api<u8> for JsValue {
+    fn wire2api(self) -> u8 {
+        self.unchecked_into_f64() as _
+    }
+}
+impl Wire2Api<Vec<u8>> for JsValue {
+    fn wire2api(self) -> Vec<u8> {
+        self.unchecked_into::<js_sys::Uint8Array>().to_vec().into()
+    }
+}
